03c4ec9e2790c5b9037274b00c00707fc4e01543
Hong Wei Tai <e1138406@u.nus.edu>
2025-03-08 21:13:23 +0800

    Add error handling

--- routes/student_cgpa_timeframe.py
+++ routes/student_cgpa_timeframe.py
@@ -6,20 +6,30 @@ from routes.api_blueprint import api_bp
 def get_students_cgpa_timeframe(start_sem, end_sem):
     students = Student.query.all()
 
+    if not students:
+        return jsonify({"Error": "No students found"}), 404
+
     students_list = []
     for s in students:
         start_sem_obj = Semester.query.filter_by(semester_name=start_sem).first()
         end_sem_obj = Semester.query.filter_by(semester_name=end_sem).first()
         teacher_name = Teacher.query.with_entities(Teacher.name).filter_by(id=s.teacher_id).first()[0]
-        
+
+        if not start_sem_obj or not end_sem_obj:
+            return jsonify({"Error": "Invalid semester names provided"}), 400
+
         # Filter grade based on timeframe
         grades = Grade.query.filter(
             (Grade.student_id == s.id) & 
             (Grade.semester_id >= start_sem_obj.id) &
             (Grade.semester_id <= end_sem_obj.id)
-        )
+        ).all()
         
-        cgpa = round(sum(grade.gpa for grade in grades) / grades.count(), 2)
+        if not grades:
+            cgpa = None
+            return jsonify({"Error": "Invalid timeframe selected."}), 400
+        else:
+            cgpa = round(sum(grade.gpa for grade in grades) / len(grades), 2)
         # Append dict to list
         students_list.append({
             "Name": s.name,
