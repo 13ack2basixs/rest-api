580aa59b9ff22ab3250cbd58bfcdfb957d263f69
Hong Wei Tai <e1138406@u.nus.edu>
2025-03-08 21:03:08 +0800

    Clean code

--- routes/students_routes.py
+++ routes/students_routes.py
@@ -8,6 +8,9 @@ def get_students():
     students = Student.query.all()
     gpas = Grade.query.all()
 
+    if not students:
+        return jsonify({"error": "No students found"}), 404
+
     # {student.id: gpa}
     cgpa_dict = {} 
     for g in gpas:
@@ -19,9 +22,9 @@ def get_students():
     for s in students:
         teacher_name = Teacher.query.with_entities(Teacher.name).filter_by(id=s.teacher_id).first()[0]
         # {student.name: ___, teacher's name: ___, cgpa: ___}
-        student_dict = {}
-        student_dict["Name"] = s.name
-        student_dict["Teacher's Name"] = teacher_name
-        student_dict["cGPA"] = round(cgpa_dict[s.id] / 8.0, 2)
-        students_list.append(student_dict)
+        students_list.append({
+            "Name": s.name,
+            "Teacher's Name": teacher_name,
+            "cGPA": round(cgpa_dict[s.id] / 8.0, 2)
+        })
     return jsonify(students_list)
