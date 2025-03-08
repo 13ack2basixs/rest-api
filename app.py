6ebc4cf6f411fc3c39fb574db2f46880e77be216
Hong Wei Tai <e1138406@u.nus.edu>
2025-03-08 18:24:43 +0800

    Connect to Neon

--- app.py
+++ app.py
@@ -1,9 +1,17 @@
 from flask import Flask
 from models import db
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 app = Flask(__name__)
 
-# Initialise database
+# Connect to my Neon database
+app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv('DATABASE_URL')
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+# Initialise database and register API blueprint 
 db.init_app(app)
 
 if __name__ == '__main__':
