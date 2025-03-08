ce73a22e07776489ec90da0803692e8703328a54
Hong Wei Tai <e1138406@u.nus.edu>
2025-03-08 20:59:06 +0800

    Clean code

--- routes/student_cgpa_timeframe.py
+++ routes/student_cgpa_timeframe.py
@@ -8,18 +8,22 @@ def get_students_cgpa_timeframe(start_sem, end_sem):
 
     students_list = []
     for s in students:
+        start_sem_obj = Semester.query.filter_by(semester_name=start_sem).first()
+        end_sem_obj = Semester.query.filter_by(semester_name=end_sem).first()
+        teacher_name = Teacher.query.with_entities(Teacher.name).filter_by(id=s.teacher_id).first()[0]
+        
         # Filter grade based on timeframe
         grades = Grade.query.filter(
             (Grade.student_id == s.id) & 
-            (Grade.semester_id >= Semester.query.filter_by(semester_name=start_sem).first().id) &
-            (Grade.semester_id <= Semester.query.filter_by(semester_name=end_sem).first().id)
+            (Grade.semester_id >= start_sem_obj.id) &
+            (Grade.semester_id <= end_sem_obj.id)
         )
         
         cgpa = round(sum(grade.gpa for grade in grades) / grades.count(), 2)
-        # Create dict for each student
-        student_dict = {}
-        student_dict["Name"] = s.name
-        student_dict["Teacher's Name"] = Teacher.query.with_entities(Teacher.name).filter_by(id=s.teacher_id).first()[0]
-        student_dict["cGPA"] = cgpa
-        students_list.append(student_dict)
+        # Append dict to list
+        students_list.append({
+            "Name": s.name,
+            "Teacher's Name": teacher_name,
+            "cGPA": cgpa
+        })
     return jsonify(students_list)
--- routes/students_routes.py
+++ routes/students_routes.py
@@ -17,10 +17,11 @@ def get_students():
 
     students_list = []
     for s in students:
+        teacher_name = Teacher.query.with_entities(Teacher.name).filter_by(id=s.teacher_id).first()[0]
         # {student.name: ___, teacher's name: ___, cgpa: ___}
         student_dict = {}
         student_dict["Name"] = s.name
-        student_dict["Teacher's Name"] = Teacher.query.with_entities(Teacher.name).filter_by(id=s.teacher_id).first()[0]
+        student_dict["Teacher's Name"] = teacher_name
         student_dict["cGPA"] = round(cgpa_dict[s.id] / 8.0, 2)
         students_list.append(student_dict)
     return jsonify(students_list)
