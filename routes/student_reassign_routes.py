5d5af2ec484166b4f8e93f02912666a501494759
Hong Wei Tai <e1138406@u.nus.edu>
2025-03-08 22:18:04 +0800

    Add error handling

--- routes/student_reassign_routes.py
+++ routes/student_reassign_routes.py
@@ -6,7 +6,11 @@ from routes.api_blueprint import api_bp
 @api_bp.route('/students/<int:student_id>/reassign', methods=['PUT'])
 def reassign_teacher(student_id):
     stud = Student.query.filter_by(id=student_id).first()
+    if not stud:
+        return jsonify({"Error": "Invalid student ID"}), 400
     other_teacher = Teacher.query.filter(Teacher.id != stud.teacher_id).first()
+    if not other_teacher:
+        return jsonify({"Error": "No available teachers for reassignment"}), 400
     stud.teacher_id = other_teacher.id
     db.session.commit()
 
